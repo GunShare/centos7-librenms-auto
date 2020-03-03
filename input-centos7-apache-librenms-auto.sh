eddf2c3a9f29c5d289996fa679fd7873ee471275
GunShare <x2163126@gmail.com>
2020-03-04 00:23:33 +0800

    Update input-centos7-apache-librenms-auto.sh
    fix timezone error php.ini

--- input-centos7-apache-librenms-auto.sh
+++ input-centos7-apache-librenms-auto.sh
@@ -49,6 +49,8 @@ echo [mariadb-5.5] >> /etc/my.cnf.d/server.cnf
 systemctl restart mariadb
 
 #設定Web Server
+#多設定一行避免寫入到MARK，;END
+echo      >> /etc/php.ini
 echo date.timezone = \"Asia/Taipei\" >> /etc/php.ini
 
 #設定apache
